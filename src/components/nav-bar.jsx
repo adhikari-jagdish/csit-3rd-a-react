a7fd25366ce6c5f9c3bf58973bb113e41e45f311
Jagadish <adhikari.jagdish5200@gmail.com>
2025-03-10 09:49:32 +0545

    Changes in CSS

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,21 +1,22 @@
 import { Link, Outlet } from "react-router-dom";
 import './home.css';
 const NavBar = () => {
+
     return (
         <>
-        <nav className="navbar">
-            <div className="logo">Movies App</div>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/movies">Movies</Link>
-                <Link to="/about-us">About Us</Link>
-                <Link to="/contact-us">Contact Us</Link>
-            </div>
-           
-        </nav>
-        <Outlet/>
+            <nav className="navbar">
+                <div className="logo">Movies App</div>
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/movies">Movies</Link>
+                    <Link to="/about-us">About Us</Link>
+                    <Link to="/contact-us">Contact Us</Link>
+                </div>
+                
+            </nav>
+            <Outlet />
         </>
-        
+
     )
 }
 
